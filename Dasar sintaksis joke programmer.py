f0088e4ca18a52d1d6329045982444160a34e6d6
kun7 <lanang7104@gmail.com>
2021-08-27 06:25:27 +0700

    Dasar sintaksis

--- Dasar sintaksis joke programmer.py
+++ Dasar sintaksis joke programmer.py
@@ -13,9 +13,15 @@ print('Virtue start shopping')
 
 # Percabangan
 sum_milk_bottle = 100
-
+sum_egg = 1574
+print(f'milk bottle available {sum_milk_bottle} bottle')
+print(f'egg available {sum_egg} egg' )
 if sum_milk_bottle > 0:
     print('Virtue buy milk')
+    if sum_egg >6:
+        print('Virtue buy 6 egg')
+    else:
+        print('Virtue does not buy egg')
 else:
     print('Virtue does not buy a milk')
 
