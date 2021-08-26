c02b7983515b61d76b06e856189a183966330703
kun7 <lanang7104@gmail.com>
2021-08-26 16:30:37 +0700

    sequential and branching practice

---
+++ maen.py
@@ -0,0 +1,22 @@
+"""
+Semua sintaksis dasar terdiri dari:
+1. sekuensial : langkah berurutan
+2. percabangan : langkah melompat jika kondisi terpenuhi
+3. pengulangan : mengulang langkah yang sama berkali-kali sampai kondisi terpenuhi
+"""
+# sequential
+print("mom said this and that")
+print('Virtue replied "yes, mom"')
+print('mom said" buy me 1 milk and if there is an egg, buy 6')
+print(' Virtue goes to the market')
+print('Virtue start shopping')
+
+# Percabangan
+sum_milk_bottle = 100
+
+if sum_milk_bottle > 0:
+    print('Virtue buy milk')
+else:
+    print('Virtue does not buy a milk')
+
+print('Virtue goes home and give the groceries to mom')
