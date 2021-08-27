e41c06f440a01b98f69c9dc8c2561cf45e39a9a2
kun7 <lanang7104@gmail.com>
2021-08-27 14:09:55 +0700

    Dasar sintaksis perulangan

---
+++ Dasar_sintaksis_perulangan_buku_dengan_while.py
@@ -0,0 +1,10 @@
+jumlah_buku = 1050
+print('Ibu berkata "Baca semua buku "')
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    print("baca satu buku yang belum dibaca")
+    jumlah_buku_yang_sudah_dibaca +=1
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku}")
