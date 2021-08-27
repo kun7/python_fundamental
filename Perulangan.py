1e3fad0ad848c94514c8882721975a6379e29422
kun7 <lanang7104@gmail.com>
2021-08-27 13:52:00 +0700

    Dasar sintaksis perulangan

--- Perulangan.py
+++ Perulangan.py
@@ -3,7 +3,7 @@ print('Ibu berkata "Baca semua buku "')
 jumlah_buku_yang_sudah_dibaca = 0
 print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
 
-for i in range(jumlah_buku_yang_sudah_dibaca, jumlah_buku+1):
-    jumlah_buku_yang_sudah_dibacabuku_yang_sudah_dibaca=+1
-    print(f"buku ke {jumlah_buku_yang_sudah_dibacabuku_yang_sudah_dibaca} sudah dibaca")
-print(f"Jumlah buku yang sudah dibaca {buku_yang_sudah_dibaca}")
+for i in range(jumlah_buku_yang_sudah_dibaca, jumlah_buku):
+    jumlah_buku_yang_sudah_dibaca +=1
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku}")
