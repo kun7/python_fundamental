d79b7d97dc229fc79b81925b28d6142726c4e8c8
kun7 <lanang7104@gmail.com>
2021-08-28 08:22:58 +0700

    Dasar fungsi while

---
+++ Dasar_sintaksis_tipe_data_list.py
@@ -0,0 +1,26 @@
+book_list = ['Malcolm Galdwell', 'Andy Newman', 'Angela Duckworth', 'Mihaly Csikszentmihalyi']
+print('\nShow the book list')
+print(book_list)
+
+for i in book_list:
+    print(i)
+
+print('\nShow the List of content')
+print(book_list[0])
+print(book_list[3])
+
+book_list.append('William Atkins')
+for i in range(0,len(book_list)):
+    print(i)
+
+#book_list.clear()
+#print(book_list)
+
+#ambil elemen ketiga
+book_list[1] = 'Ramadhan said al Buthy'
+book_list.pop(2)
+print(book_list)
+
+#ambil elemen terakhir
+book_list.pop()
+print(book_list)
